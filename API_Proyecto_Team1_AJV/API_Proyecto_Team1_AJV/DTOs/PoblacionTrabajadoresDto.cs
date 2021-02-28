398c97af4dd14926a6a18143f7050e1b26a194a2
Víctor Alfonso Martí <victor19am@gmail.com>
2021-02-28 21:34:19 +0100

    API Y FRONT Página Graphs creada con data en local y gràficos estáticos, falta paginación e implementar estilos al index.html 28/02/2021

---
+++ API_Proyecto_Team1_AJV/API_Proyecto_Team1_AJV/DTOs/PoblacionTrabajadoresDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Proyecto_Team1_AJV.DTOs
+{
+    public class PoblacionTrabajadoresDto
+    {
+        /*
+         *  DTO para recoger en el front el nombre de las poblaciones y provincias de cada trabajador para los gráficos.
+         */
+
+        // ATRIBUTOS, GETTERS Y SETTERS
+        public string D_Provincia { get; set; }
+        public string D_Poblacion { get; set; }
+    }
+}
