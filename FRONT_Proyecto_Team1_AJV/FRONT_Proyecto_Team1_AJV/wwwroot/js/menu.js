1f98c0939f9104880fffc5321d5d7d3414ddc7b5
Jepolancodlc <Jepolancodlc@gmail.com>
2021-03-04 01:08:28 +0100

    Funciones nuevas y algunos retoques

--- FRONT_Proyecto_Team1_AJV/FRONT_Proyecto_Team1_AJV/wwwroot/js/menu.js
+++ FRONT_Proyecto_Team1_AJV/FRONT_Proyecto_Team1_AJV/wwwroot/js/menu.js
@@ -3,30 +3,20 @@
 
 // Write your JavaScript code.
 
-function ocultarMenuFiltros() {
-    // OCULTA/MUESTRA EL MENU DE FILTROS
-    let x = document.getElementById("colMenuFiltros");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+$(document).ready(function () {
+    //MUESTRA/OCULTA LA COLUMNA DE FILTROS
+    $("#ico").click(function () {
+        $("#colMenuFiltros").toggle();
+    });
+
+});
+
+$(document).ready(function () {
+    // MUESTRA/OCULTA EL NOMBRE DE LAS PESTAÑAS DEL SIDEBAR
+        $("#btnHide").click(function () {
+            $(".lblNames").slideToggle();
+        });
+});
 
 
 
-function ocultaSBnombres() {
-    // OCULTA/MUESTRA LOS NOMBRES DEL SIDEBAR
-    let elements = document.getElementsByClassName("lblNames");
-
-    for (let i = 0; i < elements.length; i++) {
-
-        if (elements[i].style.display == "inline") {
-            elements[i].style.display = "none";
-        }
-        else {
-            elements[i].style.display = "inline";
-        }
-    }
-
-}
